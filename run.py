ec96dd9822044f717b39e5c70a3e5c040e5d85c7
Stephen Roca <sroca@manh.com>
2014-12-02 20:14:59 -0500

    adding all other changes made today

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 from generate_summaries import *
-
+from educreator import *
+#create_edus("./topics/battery-life_amazon_kindle.txt.data", "battery_life.edus")
+create_edus("./topics/room_holiday_inn_london.txt.data", "hotel_service.edus")
 generate_summaries("./summary")
 
--- test.py
+++ test.py
@@ -19,3 +19,12 @@ r.model_filename_pattern = "battery-life_amazon_kindle.[A-Z].#ID#.gold"
 output = r.convert_and_evaluate()
 print(output)
 output_dict = r.output_to_dict(output)
+
+
+r.system_dir = "./summary"
+r.model_dir = "./summaries-gold/room_holiday_inn_london"
+r.system_filename_pattern = "hotel_service.(\d+).summary"
+r.model_filename_pattern = "room_holiday_inn_london.[A-Z].#ID#.gold"
+output = r.convert_and_evaluate()
+print(output)
+output_dict = r.output_to_dict(output)
